83b2dd0925c65face831a0ac28e3634245b93607
Nikita Kuchko <nikita.kuchko@yellow.systems>
2021-10-14 15:50:06 +0300

    Change Footer component

--- HOC/Layout/Footer/Footer.tsx
+++ HOC/Layout/Footer/Footer.tsx
@@ -2,12 +2,12 @@ import cn from 'classnames';
 import { format } from 'date-fns';
 
 import { FooterProps } from './Footer.props';
-import styles from './Footre.model.css';
+import styles from './Footer.module.css';
 
 export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
 		<div className={cn(styles.footer, className)} {...props}>
-			<p>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</p>
+			<span>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</span>
 			<a href="#" target="_blank">Пользовательское соглашение</a>
 			<a href="#" target="_blank">Политика конфиденциальности</a>
 		</div>
