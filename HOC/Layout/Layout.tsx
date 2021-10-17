6caff0f864d0d765ec463626c0dc0c040ee8cbaa
Nikita Kuchko <nikita.kuchko@yellow.systems>
2021-10-17 23:59:01 +0300

    Add Menu component

--- HOC/Layout/Layout.tsx
+++ HOC/Layout/Layout.tsx
@@ -1,11 +1,11 @@
 import { FunctionComponent } from 'react';
-import cn from 'classnames';
 
 import { Header } from "./Header/Header";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Footer } from "./Footer/Footer";
 import { LayoutProps } from './Layout.props';
 import styles from './Layout.module.css';
+import { AppContextProvider, IAppContext } from '../../context/app.context';
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
@@ -20,12 +20,14 @@ export const Layout = ({ children }: LayoutProps): JSX.Element => {
 	);
 };
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
 	return function withLayoutComponent(props: T): JSX.Element {
 		return (
-			<Layout>
-				<Component {...props} />
-			</Layout>
-		)
-	}
-}
+			<AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+				<Layout>
+					<Component {...props} />
+				</Layout>
+			</AppContextProvider>
+		);
+	};
+};
--- HOC/Layout/Sidebar/Sidebar.tsx
+++ HOC/Layout/Sidebar/Sidebar.tsx
@@ -1,11 +1,15 @@
-import { SidebarProps } from './Sidebar.props';
-import styles from './Sidebar.props';
 import cn from 'classnames';
+import { SidebarProps } from './Sidebar.props';
+import { Menu } from '../Menu/Menu';
+import Logo from './logo.svg';
+import styles from './Sidebar.module.css';
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
 	return (
-		<div {...props}>
-			Sidebar
+		<div className={cn(className, styles.sidebar)} {...props}>
+			<Logo className={styles.logo} />
+			<div>поиск</div>
+			<Menu />
 		</div>
 	);
 };
