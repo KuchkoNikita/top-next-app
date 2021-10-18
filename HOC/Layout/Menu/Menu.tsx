66c43d8fb7595d97404fb69bb7afbea0ea242332
Nikita Kuchko <nikita.kuchko@yellow.systems>
2021-10-18 12:18:03 +0300

    Trensfer firstLevelMenu in helpers

--- HOC/Layout/Menu/Menu.tsx
+++ HOC/Layout/Menu/Menu.tsx
@@ -5,19 +5,8 @@ import cn from 'classnames';
 import styles from './Menu.module.css';
 import { AppContext } from '../../../context/app.context';
 import { FirstLevelMenuItem, PageItem } from '../../../interfaces/menu.interface';
-import { TopLevelCategory } from '../../../interfaces/page.interface';
-import CoursesIcon from './icons/courses.svg';
-import ServicesIcon from './icons/services.svg';
-import BooksIcon from './icons/books.svg';
-import ProductsIcon from './icons/products.svg';
+import { firstLevelMenu } from "../../../helpers/helpers"
 import { useRouter } from 'next/router';
-
-const firstLevelMenu: FirstLevelMenuItem[] = [
-	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products }
-];
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
