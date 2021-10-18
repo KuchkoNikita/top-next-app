1f344e867787af8578373395739ce46c394e77cd
Nikita Kuchko <nikita.kuchko@yellow.systems>
2021-10-18 22:00:39 +0300

    Uncomment HhData component in TopPageComponent

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -18,7 +18,7 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 				<Htag tag='h2'>Вакансии - {page.category}</Htag>
 				<Tag color='red' size='m'>hh.ru</Tag>
 			</div>
-			{/* {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />} */}
+			{firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
 		</div>
 	);
 };
